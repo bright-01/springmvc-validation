185645ec6b3d360abae290c69f65429b591d1f0f
mwchoen <mwcheon@cudo.co.kr>
2022-06-25 23:28:18 +0900

    bean validation - v4

---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -0,0 +1,140 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/v4/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV4 {
+
+    private final ItemRepository itemRepository;
+
+    // ValidationItemControllerv3 컨트롤러가 호출 될때마다 initBainder가 호출된다
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation/v4/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v4/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation/v4/addForm";
+    }
+
+    /**
+     * 스프링 MVC는 어떻게 Bean Validator를 사용?
+     * 스프링 부트가 spring-boot-starter-validation 라이브러리를 넣으면 자동으로 Bean Validator를
+     * 인지하고 스프링에 통합한다.
+     * 스프링 부트는 자동으로 글로벌 Validator로 등록한다.
+     * LocalValidatorFactoryBean 을 글로벌 Validator로 등록한다. 이 Validator는 @NotNull 같은
+     * 애노테이션을 보고 검증을 수행한다. 이렇게 글로벌 Validator가 적용되어 있기 때문에, @Valid ,
+     * @Validated 만 적용하면 된다.
+     * 검증 오류가 발생하면, FieldError , ObjectError 를 생성해서 BindingResult 에 담아준다.
+     * */
+    @PostMapping("/add1") // @Validated 를 넣으면 알아서 동작함
+    public String addItem1(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        extracted(item, bindingResult);
+
+
+        String errors = hasErrors(bindingResult);
+        if (errors != null) return errors;
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+    @PostMapping("/add") // @Validated 를 넣으면 알아서 동작함
+    public String addItem2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        extracted(item, bindingResult);
+
+
+        String errors = hasErrors(bindingResult);
+        if (errors != null) return errors;
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation/v4/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit1")
+    public String edit1(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+        extracted(item, bindingResult);
+
+        String errors = hasErrors(bindingResult);
+        if (errors != null) return errors;
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit2(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+        extracted(item, bindingResult);
+
+        String errors = hasErrors(bindingResult);
+        if (errors != null) return errors;
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v4/items/{itemId}";
+    }
+
+    private void extracted(Item item, BindingResult bindingResult) {
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+    }
+
+    private String hasErrors(BindingResult bindingResult) {
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v4/addForm";
+        }
+        return null;
+    }
+
+
+}
+
